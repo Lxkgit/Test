8bb5bfea50c490293d1d8209ade96e53cd234bc5
lxk <470687917@qq.com>
2024-09-11 00:09:31 +0800

    24-09-11【功能新增】：整合es并测试完成

---
+++ ElasticSearch/demo/src/main/java/com/example/demo/entity/UserModel.java
@@ -0,0 +1,16 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+public class UserModel {
+
+    private String username;
+    private String password;
+}
